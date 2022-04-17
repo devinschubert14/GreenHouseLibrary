32bb529ce92a165313bada2c785a649c21fa2e39
devinschubert14 <devinschubert@.com>
2022-04-16 22:50:32 -0700

    changed home to create

--- server/routes/books.js
+++ server/routes/books.js
@@ -25,7 +25,7 @@ recordRoutes.route("/books").get(function (req, res) {
 });
 
 // This section will help you get a single record by id
-recordRoutes.route("/book/:id").get(function (req, res) {
+recordRoutes.route("/books/:id").get(function (req, res) {
   let db_connect = dbo.getDb();
   let myquery = { _id: ObjectId( req.params.id )};
   db_connect
@@ -33,10 +33,19 @@ recordRoutes.route("/book/:id").get(function (req, res) {
       .findOne(myquery, function (err, result) {
         if (err) throw err;
         res.json(result);
-        console.log(res);
       });
 });
-
+//This section will help get a single record by id
+recordRoutes.route("/books/").get(function (req, res){
+    let db_connect = dbo.getDb();
+    let myquery = { _id: ObjectId( req.params.id )};
+    db_connect
+        .collection("records")
+        .findOne(myquery, function(err, result){
+            if(err) throw err;
+            res.json(result);
+        });
+});
 // This section will help you create a new record.
 recordRoutes.route("/books/add").post(function (req, response) {
   let db_connect = dbo.getDb();
