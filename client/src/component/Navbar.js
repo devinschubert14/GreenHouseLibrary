eb870e8ca8fa1be8706f386ad39d353ca35300ec
Amy Law <amyyeelaw@gmail.com>
2022-04-16 19:13:21 -0700

    navbar

--- client/src/component/Navbar.js
+++ client/src/component/Navbar.js
@@ -1,5 +1,5 @@
 import React from "react";
- 
+
 // We import bootstrap to make our application look better.
 import "bootstrap/dist/css/bootstrap.css";
  
@@ -9,9 +9,17 @@ import { NavLink } from "react-router-dom";
 // Here, we display our Navbar
 export default function Navbar() {
  return (
-   <div>
-     <nav className="navbar navbar-expand-lg navbar-light bg-light">
-        <p>Hello</p>
+   <div >
+     <nav className="navbar navbar-expand-lg navbar-light .bg-primary">
+        <a href = "http://www.thegreenhousecenter.org">
+          <img className = "navbar-brand" src={require('./Greenhouselogo.png')}/>
+        </a>
+          <p class = "home-icon">
+            <button type="button" class="btn btn-outline-success btn-lg" data-bs-toggle="button" autocomplete="off">Home</button>
+          </p>
+          <p class = "admin-icon">
+            <button type="button" class="btn btn-outline-success btn-lg" data-bs-toggle="button" autocomplete="off">Admin</button>
+          </p>
      </nav>
    </div>
  );
