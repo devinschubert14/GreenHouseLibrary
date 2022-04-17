8881ccbc774b82ad3138e86a8d250b0f61b09695
erin <ejle@ucdavis.edu>
2022-04-17 11:00:31 -0700

    Search Bar

--- client/src/component/Home.js
+++ client/src/component/Home.js
@@ -1,15 +1,21 @@
 import React from "react";
 import Create from './Create';
+//import DisplayResults from './searchResults';
 import Admin from './Admin'
+import SearchBar from './SearchBar';
 // We import bootstrap to make our application look better.
 import "bootstrap/dist/css/bootstrap.css";
- 
+
 // We import NavLink to utilize the react router.
 import { NavLink } from "react-router-dom";
 import Navbar from "./Navbar"
 // Here, we display our Navbar
 export default function Home() {
  return (
+	 <div>
      <Create />
+		 <SearchBar />
+	</div>
+
  );
 }
