e54ff6975cb8e1bf6ab1ffb698db5150d584becd
erin <ejle@ucdavis.edu>
2022-04-17 10:04:26 -0700

    added searchResults.js, edited App.js to include it

--- client/src/App.js
+++ client/src/App.js
@@ -1,13 +1,14 @@
 import React from "react";
- 
+
 // We use Route in order to define the different routes of our application
 import { Route, Routes } from "react-router-dom";
- 
+
 // We import all the components we need in our app
 import Navbar from "./component/Navbar";
 import Create from "./component/Create";
 import Admin from "./component/Admin"
 import Home from "./component/Home";
+import DisplayResults from "./component/searchResults";
 const App = () => {
  return (
    <div>
@@ -17,9 +18,10 @@ const App = () => {
        <Route path="/admin" element={<Admin />} />
        <Route path="/create" element={<Create />} />
        <Route path="/admin" element={<Admin />} />
+			 <Route path="/search-results" element = {<DisplayResults />} />
      </Routes>
    </div>
  );
 };
- 
+
 export default App;
--- client/src/index.js
+++ client/src/index.js
@@ -2,14 +2,19 @@ import React from "react";
 import {createRoot} from "react-dom/client";
 import App from "./App";
 import { BrowserRouter } from "react-router-dom";
+import Navbar from "./component/Navbar";
+
 
 const container = document.getElementById("root")
 const root = createRoot(container)
+
+
 root.render(
   <React.StrictMode>
     <BrowserRouter>
       <App />
+
     </BrowserRouter>
   </React.StrictMode>,
-  
+
 );
